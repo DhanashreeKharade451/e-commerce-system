7178ded9dd7f5e6e0d212b49f83978c87b30c0f0
Dhanashree <dhanashreeakharade@gmail.com>
2026-02-06 05:50:20 -0500

    fetched data in API servises

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,19 +1,21 @@
-async function fetchAPIData() {
+
+ async function fetchAPIData() {
     try {
         const response = await fetch('https://dummyjson.com/products');
         const data = await response.json();
-
         for (let product of data.products) {
             console.log('id: ' + product.id + ', title: ' + product.title);
         }
-
         console.log('totalProducts:', data.products.length);
     } catch (e) {
         console.log(e);
+        throw e;
     }
 }
 
 fetchAPIData();
+
+
 
 // fetch('https://dummyjson.com/products')
 // .then(res => res.json())

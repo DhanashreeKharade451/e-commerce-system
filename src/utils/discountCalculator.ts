32219dde158c850e3ba42e70c0a6e820e1aa1510
Dhanashree <dhanashreeakharade@gmail.com>
2026-02-06 09:22:04 -0500

    calculateTax function is working fine

--- src/utils/discountCalculator.ts
+++ src/utils/discountCalculator.ts
@@ -1,7 +1,14 @@
 
-export function calculateDiscount(price, discountPercentage) {
+
+
+ function calculateDiscount(price:number, discountPercentage:number) {
   return (price * discountPercentage) / 100;
 }
+
+console.log(calculateDiscount(100, 10)); // 10
+const discount = calculateDiscount(200, 15); // 30
+console.log(discount);
+
 
 
 
--- src/utils/taxCalculator.ts
+++ src/utils/taxCalculator.ts
@@ -1,10 +1,23 @@
 
-export function calculateTax(price, category) {
+function calculateTax(price:number, category:string) {
   let taxRate = 0.0475; // default 4.75%
-
-  if (category === "groceries") {
-    taxRate = 0.03; // groceries tax
+    if (category === "groceries") {
+      taxRate = 0.03; // groceries tax
+    }
+    return price * taxRate;
   }
 
-  return price * taxRate;
-}
+  console.log(calculateTax(100, "electronics")); // 4.75
+  const tax = calculateTax(200, "groceries");
+  console.log(tax);
+
+
+// export function calculateTax(price, category) {
+//   let taxRate = 0.0475; // default 4.75%
+
+//   if (category === "groceries") {
+//     taxRate = 0.03; // groceries tax
+//   }
+
+//   return price * taxRate;
+// }
